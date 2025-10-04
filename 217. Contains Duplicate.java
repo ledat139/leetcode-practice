6a10d342e41e855ba02519f63043ac5f8b2cfd50
Lê Tiến Đạt <158796165+ledat139@users.noreply.github.com>
2025-10-04 17:06:07 +0700

    217. Contains Duplicate

---
+++ 217. Contains Duplicate.java
@@ -0,0 +1,13 @@
+import java.util.HashMap;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashMap<Integer, Integer> store = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (store.containsKey(nums[i])) {
+                return true;
+            }
+            store.put(nums[i], 1);
+        }
+        return false;
+    }
+}
