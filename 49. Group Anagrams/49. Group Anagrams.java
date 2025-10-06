3aafdf1a45983a93a52fad6259936a45a39fde38
Lê Tiến Đạt <158796165+ledat139@users.noreply.github.com>
2025-10-06 18:22:05 +0700

    49. Group Anagrams

---
+++ 49. Group Anagrams/49. Group Anagrams.java
@@ -0,0 +1,17 @@
+class Solution {
+  public List<List<String>> groupAnagrams(String[] strs) {
+    HashMap<String, List<String>> map = new HashMap<>();
+
+    for (int i = 0; i < strs.length; i++) {
+      char[] chars = strs[i].toCharArray();
+      Arrays.sort(chars);
+      String sorted = new String(chars);
+      if (!map.containsKey(sorted)) {
+        map.put(sorted, new ArrayList<>());
+      }
+      map.get(sorted).add(strs[i]);
+    }
+    return new ArrayList<>(map.values());
+  }
+}
+// Lặp qua mảng string, sort phần tử và lấy đó làm key, nếu sort của phần tử nào trùng với key thì put vào hashmap
